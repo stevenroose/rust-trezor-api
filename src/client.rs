1c7fd53fb3d08bcc0f6de1b68298c93d8d9459ef
Steven Roose <steven@stevenroose.org>
2018-11-20 00:20:37 +0000

    Implement PassphraseStateRequest as extra Interaction
    This is not supposed to be implemented this way, but I couldn't find
    another way to do it, unfortunately.  Once Box<FnOnce> is possible or
    FnBox becomes stable, we might be able to do this.

--- src/client.rs
+++ src/client.rs
@@ -37,6 +37,7 @@ pub enum InteractionType {
 	Button,
 	PinMatrix,
 	Passphrase,
+	PassphraseState,
 }
 
 //TODO(stevenroose) should this be FnOnce and put in an FnBox?
@@ -136,6 +137,32 @@ impl<'a, T, R: TrezorMessage> PassphraseRequest<'a, T, R> {
 	}
 }
 
+/// A passphrase state request message sent by the device.
+pub struct PassphraseStateRequest<'a, T, R: TrezorMessage> {
+	message: protos::PassphraseStateRequest,
+	client: &'a mut Trezor,
+	result_handler: Box<ResultHandler<'a, T, R>>,
+}
+
+impl<'a, T, R: TrezorMessage> fmt::Debug for PassphraseStateRequest<'a, T, R> {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		fmt::Debug::fmt(&self.message, f)
+	}
+}
+
+impl<'a, T, R: TrezorMessage> PassphraseStateRequest<'a, T, R> {
+	/// The passphrase state provided by the device.
+	pub fn passphrase_state(&self) -> &[u8] {
+		self.message.get_state()
+	}
+
+	/// Ack the receipt of the passphrase state.
+	pub fn ack(self) -> Result<TrezorResponse<'a, T, R>> {
+		let req = protos::PassphraseStateAck::new();
+		self.client.call(req, self.result_handler)
+	}
+}
+
 /// A response from a Trezor device.  On every message exchange, instead of the expected/desired
 /// response, the Trezor can ask for some user interaction, or can send a failure.
 #[derive(Debug)]
@@ -145,6 +172,10 @@ pub enum TrezorResponse<'a, T, R: TrezorMessage> {
 	ButtonRequest(ButtonRequest<'a, T, R>),
 	PinMatrixRequest(PinMatrixRequest<'a, T, R>),
 	PassphraseRequest(PassphraseRequest<'a, T, R>),
+	//TODO(stevenroose) This should be taken out of this enum and intrinsically attached to the 
+	// PassphraseRequest variant.  However, it's currently impossible to do this.  It might be
+	// possible to do with FnBox (currently nightly) or when Box<FnOnce> becomes possible.
+	PassphraseStateRequest(PassphraseStateRequest<'a, T, R>),
 }
 
 impl<'a, T, R: TrezorMessage> fmt::Display for TrezorResponse<'a, T, R> {
@@ -155,6 +186,7 @@ impl<'a, T, R: TrezorMessage> fmt::Display for TrezorResponse<'a, T, R> {
 			TrezorResponse::ButtonRequest(ref r) => write!(f, "ButtonRequest: {:?}", r),
 			TrezorResponse::PinMatrixRequest(ref r) => write!(f, "PinMatrixRequest: {:?}", r),
 			TrezorResponse::PassphraseRequest(ref r) => write!(f, "PassphraseRequest: {:?}", r),
+			TrezorResponse::PassphraseStateRequest(ref r) => write!(f, "PassphraseStateRequest: {:?}", r),
 		}
 	}
 }
@@ -174,6 +206,9 @@ impl<'a, T, R: TrezorMessage> TrezorResponse<'a, T, R> {
 			TrezorResponse::PassphraseRequest(_) => {
 				Err(Error::UnexpectedInteractionRequest(InteractionType::Passphrase))
 			}
+			TrezorResponse::PassphraseStateRequest(_) => {
+				Err(Error::UnexpectedInteractionRequest(InteractionType::PassphraseState))
+			}
 		}
 	}
 
@@ -188,6 +223,9 @@ impl<'a, T, R: TrezorMessage> TrezorResponse<'a, T, R> {
 			}
 			TrezorResponse::PassphraseRequest(_) => {
 				Err(Error::UnexpectedInteractionRequest(InteractionType::Passphrase))
+			}
+			TrezorResponse::PassphraseStateRequest(_) => {
+				Err(Error::UnexpectedInteractionRequest(InteractionType::PassphraseState))
 			}
 		}
 	}
@@ -204,6 +242,9 @@ impl<'a, T, R: TrezorMessage> TrezorResponse<'a, T, R> {
 			TrezorResponse::PassphraseRequest(_) => {
 				Err(Error::UnexpectedInteractionRequest(InteractionType::Passphrase))
 			}
+			TrezorResponse::PassphraseStateRequest(_) => {
+				Err(Error::UnexpectedInteractionRequest(InteractionType::PassphraseState))
+			}
 		}
 	}
 
@@ -218,6 +259,27 @@ impl<'a, T, R: TrezorMessage> TrezorResponse<'a, T, R> {
 			}
 			TrezorResponse::PinMatrixRequest(_) => {
 				Err(Error::UnexpectedInteractionRequest(InteractionType::PinMatrix))
+			}
+			TrezorResponse::PassphraseStateRequest(_) => {
+				Err(Error::UnexpectedInteractionRequest(InteractionType::PassphraseState))
+			}
+		}
+	}
+
+	/// Get the passphrase request object or an error if not `PassphraseStateRequest`.
+	pub fn passphrase_state_request(self) -> Result<PassphraseStateRequest<'a, T, R>> {
+		match self {
+			TrezorResponse::PassphraseStateRequest(r) => Ok(r),
+			TrezorResponse::Ok(_) => Err(Error::UnexpectedMessageType(R::message_type())),
+			TrezorResponse::Failure(m) => Err(Error::FailureResponse(m)),
+			TrezorResponse::ButtonRequest(_) => {
+				Err(Error::UnexpectedInteractionRequest(InteractionType::Button))
+			}
+			TrezorResponse::PinMatrixRequest(_) => {
+				Err(Error::UnexpectedInteractionRequest(InteractionType::PinMatrix))
+			}
+			TrezorResponse::PassphraseRequest(_) => {
+				Err(Error::UnexpectedInteractionRequest(InteractionType::Passphrase))
 			}
 		}
 	}
@@ -312,6 +374,15 @@ impl Trezor {
 					let req_msg = resp.take_message()?;
 					trace!("Received PassphraseRequest: {:?}", req_msg);
 					Ok(TrezorResponse::PassphraseRequest(PassphraseRequest {
+						message: req_msg,
+						client: self,
+						result_handler: result_handler,
+					}))
+				}
+				MessageType_PassphraseStateRequest => {
+					let req_msg = resp.take_message()?;
+					trace!("Received PassphraseStateRequest: {:?}", req_msg);
+					Ok(TrezorResponse::PassphraseStateRequest(PassphraseStateRequest {
 						message: req_msg,
 						client: self,
 						result_handler: result_handler,
